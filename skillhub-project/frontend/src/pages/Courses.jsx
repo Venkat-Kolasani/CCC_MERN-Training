a1a70b7a04d2a4898a7b82dfe06acd64ded4ea7e
Venkat-Kolasani <kolasanivenkat2@gmail.com>
2026-07-04 10:57:29 +0530

    frontend

--- skillhub-project/frontend/src/pages/Courses.jsx
+++ skillhub-project/frontend/src/pages/Courses.jsx
@@ -1,9 +1,73 @@
+import { useEffect, useState } from "react";
+
+import API from "../api/courseApi";
+
 function Courses() {
+
+  const [courses, setCourses] =  
+    useState([]);
+
+  useEffect(() => {
+
+    getCourses();
+
+  }, []);
+
+  async function getCourses() {
+
+    try {
+
+      const response =
+        await API.get("/courses");
+
+      setCourses(response.data);
+
+    }
+
+    catch (error) {
+
+      console.log(error);
+
+    }
+
+  }
+
   return (
+
     <div className="page-container">
+
       <h1>Available Courses</h1>
-      <p>Coming soon...</p>
+
+      <h3>  
+        Total Courses :  
+        {" "}  
+        {courses.length}  
+      </h3>
+
+      {  
+        courses.map((course) => (
+
+          <div  
+            key={course._id}  
+            className="course-item"  
+          >
+
+            📘 {course.title}
+
+            {" "}
+
+            (  
+            {course.students}  
+            Students  
+            )
+
+          </div>
+
+        ))  
+      }
+
     </div>
+
   );
 }
 
